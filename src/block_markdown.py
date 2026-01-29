ab85409826eef85b2d6e6d4c7f29275c44bb3b73
Max Shishkov <maxshishkov92@gmail.com>
2026-01-28 21:57:17 -0600

    feat: add function block_to_blocktype that identifies the type of  a markdown block

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH           = "paragraph"
+    HEADING             = "heading"
+    CODE                = "code"
+    QUOTE               = "quote"
+    UNORDERED_LIST      = "unordered_list"
+    ORDERED_LIST        = "ordered_list"
+
 def markdown_to_blocks(markdown:str) -> list[str]:
     blocks = []
     split_doc = markdown.split("\n\n")
@@ -6,4 +16,47 @@ def markdown_to_blocks(markdown:str) -> list[str]:
             blocks.append(line.strip())
             
     return blocks
-            
+                     
+def block_to_blocktype(text_block:str) -> BlockType:
+    if text_block.startswith("#"):
+        stripped = text_block.lstrip("#")
+        count = len(text_block) - len(stripped)
+        if 1 <= count <= 6 and stripped.startswith(" "):
+            return BlockType.HEADING
+    if text_block.startswith("```\n") and  text_block.endswith("\n```"):
+        return BlockType.CODE
+    
+    split_text = text_block.split("\n")
+    if text_block.startswith(">"):
+        is_blockquote = True
+        for text in split_text:
+            if not text.startswith(">"):
+                is_blockquote = False
+                break
+        if is_blockquote:
+            return BlockType.QUOTE
+    if text_block.startswith("- "):
+        is_ul = True
+        for text in split_text:
+            if not text.startswith("- "):
+                is_ul = False
+                break
+        if is_ul:
+            return BlockType.UNORDERED_LIST
+    if text_block.startswith("1. "):
+        is_ol = True
+        for index, text in enumerate(split_text):
+            if not text.startswith(f"{index + 1}. "):
+                is_ol = False
+                break
+        if is_ol:
+            return BlockType.ORDERED_LIST
+        
+        
+    for text in split_text:
+        if not text.startswith(">"):
+                is_blockquote = False
+                break
+        
+        
+    return BlockType.PARAGRAPH
